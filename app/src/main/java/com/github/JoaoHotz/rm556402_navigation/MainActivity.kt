bd9b5f1ec17149a448b1d1fee871e06289b0a3e2
JoaoHotz <joaovinicius.hotz@hotmail.com>
2026-03-25 15:42:00 -0300

    Passagem opcional de parametros na PedidosScreen

--- app/src/main/java/com/github/JoaoHotz/rm556402_navigation/MainActivity.kt
+++ app/src/main/java/com/github/JoaoHotz/rm556402_navigation/MainActivity.kt
@@ -7,12 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.github.JoaoHotz.rm556402_navigation.screens.LoginScreen
 import com.github.JoaoHotz.rm556402_navigation.screens.MenuScreen
 import com.github.JoaoHotz.rm556402_navigation.screens.PedidosScreen
@@ -38,8 +37,10 @@ class MainActivity : ComponentActivity() {
                         composable(route = "menu") {
                             MenuScreen(modifier = Modifier.padding(innerPadding), navController)
                         }
-                        composable(route = "pedidos") {
-                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
+                        composable(route = "pedidos?cliente={cliente}", arguments = listOf(
+                            navArgument("cliente"){defaultValue = "Cliente generico"}
+                        )) {
+                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController, it.arguments?.getString("cliente"))
                         }
                         composable(route = "perfil/{nome}") {
                             val nome: String? = it.arguments?.getString("nome", "Usuário Genérico")
